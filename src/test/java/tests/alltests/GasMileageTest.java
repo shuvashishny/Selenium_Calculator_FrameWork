350db85b6c147b7be87f87b9e5e486630eadcd02
shuvashish <shuvashishny@gmail.com>
2019-01-22 22:55:39 -0500

    Added first test

--- src/test/java/tests/alltests/GasMileageTest.java
+++ src/test/java/tests/alltests/GasMileageTest.java
@@ -1,4 +1,18 @@
 package tests.alltests;
 
-public class GasMileageTest {
+import org.testng.annotations.Test;
+import pages.allpages.GasMileagePage;
+import pages.allpages.HomePage;
+import tests.BaseTest;
+
+public class GasMileageTest extends BaseTest{
+
+    @Test
+    public void calculateGasMileage(){
+        page.getPage(HomePage.class)
+                .navigateToHomePage()
+                .clickLinkFromOtherCalculator("Gas Mileage Calculator");
+        page.getPage(GasMileagePage.class).fillOutInfo();
+
+    }
 }
